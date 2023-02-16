551dccf9b71302f7cfffada26210a3273c6746ab
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2023-02-16 20:18:57 +0100

    finitto

--- Ejercicios/2-Paises-API-Routing/src/app/opciones/por-continente/por-continente.component.ts
+++ Ejercicios/2-Paises-API-Routing/src/app/opciones/por-continente/por-continente.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { PaisService } from '../services/pais.service';
 
 @Component({
