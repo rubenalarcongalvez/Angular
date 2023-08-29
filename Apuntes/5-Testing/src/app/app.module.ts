d20e3257ad24b898f592cb255850f3ee741b9ad5
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2023-08-29 12:13:52 +0200

    terminar ejemplo testing

---
+++ Apuntes/5-Testing/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
